1ad8b5069c1d4cc0ca9ec987c9cf2eede92a99a2
ElisaAprendeProgramacion <93006608+ElisaAprendeProgramacion@users.noreply.github.com>
2023-07-26 17:13:36 +0200

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,13 @@
+def write_story():
+    noun = input("Please add a noun: ")
+    verb = input("Please add a verb: ")
+    verb2 = input("Please add a verb: ")
+    noun2 = input("Please add a noun: ")
+    # print(noun)
+    # print(verb)
+    print(f"""
+    Once upon a time, {noun} decided to go to the sea and try {verb}.
+    It was a very nice sea, because {noun} decided to {verb2}, until {noun} met {noun2} and make friends.
+    """)
+
+write_story()
